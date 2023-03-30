f30ff0dc51c6cb6c5591498b27cea40b4782f303
Logan Hylton <lhyltoncs@gmail.com>
2023-03-30 09:31:03 -0400

    Add Auth0 athentication and navbar functionality

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,7 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import NavBar from './components/NavBar.vue';
 </script>
 
 <template lang="pug">
-header
-  nav.navbar.navbar-expand-lg.navbar-light.bg-light
-    div.navbar-brand()
-    RouterLink.navbar-brand(to='/') GCEAS EMT Class
-    button.navbar-toggler(type='button' data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation')
-      span.navbar-toggler-icon
-    #navbarSupportedContent.collapse.navbar-collapse
-      ul.navbar-nav.mr-auto
-        li.nav-item.active
-          RouterLink.nav-link(to='/') Home 
-        li.nav-item
-          RouterLink.nav-link(to='/admin') Admin
-RouterView
+NavBar
 </template>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,24 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createAuth0 } from "@auth0/auth0-vue";
+import authConfig from "../auth-config.json";
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap/dist/js/bootstrap.js';
 
 import './assets/main.css'
 
 const app = createApp(App)
-
 app.use(router)
+app.use(
+  createAuth0({
+    domain: authConfig.domain,
+    clientId: authConfig.clientId,
+    authorizationParams: {
+      redirect_uri: window.location.origin,
+    }
+  })
+)
 
 app.mount('#app')
